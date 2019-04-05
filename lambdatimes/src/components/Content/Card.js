29795a0fe651c264ff3dcad59b33ebd8dc6b7f08
cammac <camm@rocktechcomputer.com>
2019-04-05 09:24:43 -0700

    completed cards section

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Card = props => {
   return (
     <div className="card">
-      <div className="headline">{props.cards.headline}</div>
+      <div className="headline">{props.card.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={props.cards.img} />
+          <img src={props.card.img} alt='card' />
         </div>
-        <span>By {props.cards.author}</span>
+        <span>By {props.card.author}</span>
       </div>
     </div>
   );
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
 const Tab = props => {
-
-  console.log('tab', props)
+  // console.log('tab', props)
   return (
     <div
       className={props.tab === props.selectedTab ? 'tab active-tab' : 'tab'}
       onClick={props.selectTabHandler(props.tab)
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
       }
     >
       {props.tab.toUpperCase()}
