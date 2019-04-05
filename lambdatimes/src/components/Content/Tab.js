3831a90a1e3aa3ec3f09c2ffac4dbfabac2e3656
cammac <camm@rocktechcomputer.com>
2019-04-05 10:55:21 -0700

    completed tabs styled components

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -7,19 +7,56 @@ import PropTypes from 'prop-types';
 //   Component Styles
 // =======================
 
+const TabDiv = styled.div`
+  ${props => (props.tab === props.selectedTab ?
+    `display : flex;
+    justifyContent: none;
+    alignItems: center;
+    flexDirection: row;
+    margin: 0 5px;
+    padding: 2px 10px;
+    fontSize: 12px;
+    letterSpacing: 2px;
+    cursor: pointer;
+    fontWeight: bold;
+    backgroundColor: #fff;
+    color: #333;
+    border: 2px solid #333;
+    &:hover {
+      text-decoration: underline;
+    }`
+    :
+    `display : flex;
+    justifyContent: none;
+    alignItems: center;
+    flexDirection: row;
+    margin: 0 5px;
+    padding: 2px 10px;
+    fontSize: 12px;
+    letterSpacing: 2px;
+    cursor: pointer;
+    fontWeight: bold;
+    backgroundColor: #333;
+    color: #fff;
+    border: none;
+    &:hover {
+      text-decoration: underline;
+    }`
+  )}
+`;
+
 // =======================
 //     Components 
 // =======================
 
 const Tab = props => {
   return (
-    <div
-      className={props.tab === props.selectedTab ? 'tab active-tab' : 'tab'}
+    <TabDiv
       onClick={props.selectTabHandler(props.tab)
       }
     >
       {props.tab.toUpperCase()}
-    </div>
+    </TabDiv>
   );
 };
 
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -8,32 +8,32 @@ import PropTypes from 'prop-types';
 //   Component Styles
 // =======================
 
-// const TabsDiv = styled.div`
-//   border-bottom: 1px solid lightgrey;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   flex-direction: row;
-//   width: 100%;
-//   height: 47px;
-//   background-color: #fff;
-//   @media (min-width: 1280px) {
-//     width: 1280px;
-//   }
-// `;
+const TabsDiv = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+  height: 47px;
+  background-color: #fff;
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
 
-// const TopicsDiv = styled.div`
-//   display: flex;
-//   justify-content: none;
-//   align-items: center;
-//   flex-direction: row;
-// `;
+const TopicsDiv = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+`;
 
-// const TitleSpan = styled.span`
-//   font-size: 12px;
-//   letter-spacing: 1px;
-//   font-weight: bold;
-// `;
+const TitleSpan = styled.span`
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+`;
 
 // =======================
 //     Components 
@@ -41,9 +41,9 @@ import PropTypes from 'prop-types';
 
 const Tabs = props => {
   return (
-    <div className='tabs'>
-      <div className='topics'>
-        <span className='title'>TRENDING TOPICS:</span>
+    <TabsDiv>
+      <TopicsDiv>
+        <TitleSpan>TRENDING TOPICS:</TitleSpan>
 
         {props.tabs.map((item, index) =>
           <Tab
@@ -54,8 +54,8 @@ const Tabs = props => {
             selectedTab={props.selectedTab}
           />
         )}
-      </div>
-    </div>
+      </TopicsDiv>
+    </TabsDiv>
   );
 };
 
